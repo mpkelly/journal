d069eff1b910372f1559b370d1210549b9bc8cff
mkelly <kikemelly@gmail.com>
2020-06-12 12:03:33 +0700

    (fix) 404 issue when reopening offline: move the manifest to the root folder

--- packages/journal-pwa/dist/worker.js
+++ packages/journal-pwa/dist/worker.js
@@ -9,7 +9,7 @@ var urlsToCache = [
   "./public/lib/react.production.min.js",
   "./public/lib/react-dom.production.min.js",
   "./public/journal.js",
-  "./public/manifest.json",
+  "./manifest.json",
   "./worker.js",
   "./index.html",
 ];
@@ -27,25 +27,42 @@ self.addEventListener("install", (event) => {
 
 // Cache and return requests
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+  if (event.request.mode == "navigate") {
+    console.log("Handling fetch event for", event.request.url);
+    console.log(event.request);
+    event.respondWith(
+      fetch(event.request).catch(function (exception) {
+        // The `catch` is only triggered if `fetch()` throws an exception,
+        // which most likely happens due to the server being unreachable.
+        console.error(
+          "Fetch failed; returning offline page instead.",
+          exception
+        );
+        return caches.open(CACHE_NAME).then(function (cache) {
+          return cache.match("/");
+        });
+      })
+    );
+  } else {
+    // It’s not a request for an HTML document, but rather for a CSS or SVG
+    // file or whatever…
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["journal-pwa"];
+  var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log("Deleting cache", cacheName);
             return caches.delete(cacheName);
           }
         })
