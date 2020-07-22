65056f19274658792b27e58acdcee5c87cb92602
mkelly <kikemelly@gmail.com>
2020-07-22 15:53:35 +0700

    Updates

--- worker.js
+++ worker.js
@@ -1,8 +1,12 @@
 var CACHE_NAME = "journal-pwa";
 var urlsToCache = [
-  "./public/fonts/noto-sans-tc-v10-latin-700.woff2",
-  "./public/fonts/noto-sans-tc-v10-latin-regular.woff2",
-  "./public/fonts/noto-sans-tc.css",
+  "./public/fonts/noto-sans/noto-sans-tc-v10-latin-700.woff2",
+  "./public/fonts/noto-sans/noto-sans-tc-v10-latin-regular.woff2",
+  "./public/fonts/noto-sans/noto-sans-tc.css",
+  "./public/fonts/merriweather/merriweather-v21-latin-700.ttf",
+  "./public/fonts/merriweather/merriweather-v21-latin-700italic.ttf",
+  "./public/fonts/merriweather/merriweather-v21-latin-regular.ttf",
+  "./public/fonts/merriweather/merriweather-v21-latin-italic.ttf",
   "./public/icons/icon.png",
   "./public/icons/material-round.css",
   "./public/icons/MaterialRoundIconFont.woff2",
